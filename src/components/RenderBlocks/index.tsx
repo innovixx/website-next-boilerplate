c44abe70d7e076f980e3de454289d1b7d97254e8
Aman Bhogal <aman.bhogal@innovixx.co.uk>
2024-08-07 09:58:25 +0000

    chore: update renderBlock pascalCase convert

--- src/components/RenderBlocks/index.tsx
+++ src/components/RenderBlocks/index.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import * as components from '../../blocks';
 import { Page_Layout } from '../../graphql/generated/schema';
+import { toPascalCase } from '../../utils/changeCase';
 
 type Props = {
   layout: Page_Layout[];
@@ -12,7 +13,7 @@ export const RenderBlocks: React.FC<Props> = ({ layout, className }) => (
   <div className={className}>
     {layout.map((block, i) => {
       // make blockType first character uppercase
-      const blockType = `${(block.blockType ?? '').charAt(0).toUpperCase() + (block.blockType ?? '').slice(1)}Component`;
+      const blockType = `${toPascalCase(`${block.blockType}`)}Component`;
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       const Block: React.FC<any> = components[blockType as keyof typeof components];
 

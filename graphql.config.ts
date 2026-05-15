eb9da84bcdc1ced0150e40622936c2ae8ab973bb
Aman Bhogal <aman.bhogal@innovixx.co.uk>
2026-05-15 10:39:02 +0000

    chore: update graphql config to restrict document types to .graphql only

--- graphql.config.ts
+++ graphql.config.ts
@@ -2,7 +2,7 @@ import type { IGraphQLConfig } from '@graphql-eslint/eslint-plugin';
 
 const config: IGraphQLConfig = {
 	schema: 'src/graphql/generated/schema.graphql',
-	documents: 'src/**/*.{graphql,js,ts,jsx,tsx}',
+	documents: 'src/**/*.graphql',
 };
 
 export default config;
